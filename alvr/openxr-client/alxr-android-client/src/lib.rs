b304e4796e834cce6ce1f071059a728fae6917d6
korejan <64199710+korejan@users.noreply.github.com>
2023-07-23 22:02:14 +0100

    replaces from ndk-glue with android-activity

--- alvr/openxr-client/alxr-android-client/src/lib.rs
+++ alvr/openxr-client/alxr-android-client/src/lib.rs
@@ -2,73 +2,20 @@
 mod permissions;
 mod wifi_manager;
 
-use std::println;
-
-use alxr_common::{
-    alxr_destroy, alxr_init, alxr_is_session_running, alxr_on_pause, alxr_on_resume,
-    alxr_process_frame, battery_send, init_connections, input_send, path_string_to_hash,
-    request_idr, set_waiting_next_idr, shutdown, time_sync_send, video_error_report_send,
-    views_config_send, ALXRColorSpace, ALXRDecoderType, ALXRGraphicsApi, ALXRRustCtx,
-    ALXRSystemProperties, ALXRVersion, APP_CONFIG,
-};
 use permissions::check_android_permissions;
+use std::time::Duration;
+use version_compare::{Part, Version};
 use wifi_manager::{acquire_wifi_lock, release_wifi_lock};
 
-use ndk::looper::*;
-use ndk_context;
-use ndk_glue;
-use version_compare::{Part, Version};
+use android_activity::{AndroidApp, MainEvent, PollEvent};
+use android_logger;
 
-struct AppData {
-    destroy_requested: bool,
-    resumed: bool,
-}
-
-impl AppData {
-    fn handle_lifecycle_event(&mut self, event: &ndk_glue::Event) {
-        // Start,
-        // Resume,
-        // SaveInstanceState,
-        // Pause,
-        // Stop,
-        // Destroy,
-        // ConfigChanged,
-        // LowMemory,
-        // WindowLostFocus,
-        // WindowHasFocus,
-        // WindowCreated,
-        // WindowResized,
-        // WindowRedrawNeeded,
-        // WindowDestroyed,
-        // InputQueueCreated,
-        // InputQueueDestroyed,
-        // ContentRectChanged,
-        match event {
-            // ndk_glue::Event::WindowCreated => {
-            //     let win = ndk_glue::native_window();
-            //     if let Some(win2) = win.as_ref() {
-            //         let width = win2.width();
-            //         let height = win2.height();
-            //         println!("NativeWindow: width: {0}, height: {1}", width, height);
-            //     }
-            // },
-            ndk_glue::Event::Pause => {
-                println!("alxr-client: received on_pause event.");
-                self.resumed = false;
-                unsafe { alxr_on_pause() };
-                release_wifi_lock();
-            }
-            ndk_glue::Event::Resume => {
-                println!("alxr-client: received on_resume event.");
-                acquire_wifi_lock();
-                unsafe { alxr_on_resume() };
-                self.resumed = true;
-            }
-            ndk_glue::Event::Destroy => self.destroy_requested = true,
-            _ => (),
-        }
-    }
-}
+use alxr_common::{
+    alxr_destroy, alxr_init, alxr_on_pause, alxr_on_resume, alxr_process_frame, battery_send,
+    init_connections, input_send, path_string_to_hash, request_idr, set_waiting_next_idr, shutdown,
+    time_sync_send, video_error_report_send, views_config_send, ALXRColorSpace, ALXRDecoderType,
+    ALXRGraphicsApi, ALXRRustCtx, ALXRSystemProperties, ALXRVersion, APP_CONFIG,
+};
 
 fn get_build_property<'a>(jvm: &'a jni::JavaVM, property_name: &str) -> String {
     let env = jvm.attach_current_thread().unwrap();
@@ -138,125 +85,152 @@ fn get_preferred_resolution<'a>(
     None
 }
 
-#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
-pub fn main() {
-    println!("{:?}", *APP_CONFIG);
-    let mut app = AppData {
+#[no_mangle]
+fn android_main(android_app: AndroidApp) {
+    let log_level = if cfg!(debug_assertions) {
+        log::LevelFilter::Debug
+    } else {
+        log::LevelFilter::Info
+    };
+    android_logger::init_once(android_logger::Config::default().with_max_level(log_level));
+    log::info!("{:?}", *APP_CONFIG);
+    unsafe { run(&android_app).unwrap() };
+    log::info!("successfully shutdown.");
+}
+
+struct AppData {
+    destroy_requested: bool,
+    resumed: bool,
+    gained_focus: bool,
+    window_inited: bool,
+}
+
+impl AppData {
+    fn pasue(&mut self) {
+        self.resumed = false;
+        log::debug!("alxr-client: received on_pause event.");
+        unsafe { alxr_on_pause() };
+        release_wifi_lock();
+    }
+
+    fn resume(&mut self) {
+        acquire_wifi_lock();
+        log::debug!("alxr-client: received on_resume event.");
+        unsafe { alxr_on_resume() };
+        self.resumed = true;
+    }
+
+    fn handle_lifecycle_event(&mut self, event: &PollEvent) {
+        match event {
+            PollEvent::Main(main_event) => match main_event {
+                MainEvent::InitWindow { .. } => self.window_inited = true,
+                MainEvent::LostFocus => self.gained_focus = false,
+                MainEvent::GainedFocus => self.gained_focus = true,
+                MainEvent::Pause => self.pasue(),
+                MainEvent::Resume { .. } => self.resume(),
+                MainEvent::Destroy => self.destroy_requested = true,
+                _ => (),
+            },
+            // PollEvent::Wake  => (),
+            // PollEvent::Timeout => (),
+            _ => (),
+        }
+    }
+}
+
+#[inline(always)]
+fn wait_until_window_init(android_app: &AndroidApp, app_data: &mut AppData) {
+    while !app_data.destroy_requested && !app_data.window_inited {
+        log::info!("Waiting for native-window to initialize...");
+        android_app.poll_events(Some(Duration::from_millis(100)), |event| {
+            app_data.handle_lifecycle_event(&event);
+        });
+    }
+    let msg = if app_data.window_inited {
+        "successfully."
+    } else {
+        "never"
+    };
+    log::info!("native-window {msg} initialized.");
+}
+
+const NO_WAIT_TIME: Option<Duration> = Some(Duration::from_millis(0));
+
+#[inline(always)]
+unsafe fn run(android_app: &AndroidApp) -> Result<(), Box<dyn std::error::Error>> {
+    let _lib = libloading::Library::new("libopenxr_loader.so")?;
+
+    let native_activity = android_app.activity_as_ptr();
+    let vm_ptr = android_app.vm_as_ptr();
+
+    let vm = jni::JavaVM::from_raw(vm_ptr.cast())?;
+    let _env = vm.attach_current_thread()?;
+
+    check_android_permissions(native_activity as jni::sys::jobject, &vm)?;
+
+    let mut app_data = AppData {
         destroy_requested: false,
         resumed: false,
+        gained_focus: false,
+        window_inited: false,
     };
-    run(&mut app).unwrap();
-    println!("successfully shutdown.");
-    // the ndk_glue api does not automatically call this and without
-    // it main will hang on exit, currently there seems to be no plans to
-    // make it automatic, refer to:
-    // https://github.com/rust-windowing/android-ndk-rs/issues/154
-    ndk_glue::native_activity().finish();
-}
+    wait_until_window_init(&android_app, &mut app_data);
+    if app_data.destroy_requested {
+        return Ok(());
+    }
+    assert!(app_data.window_inited);
+    log::debug!("alxr-client: is activity paused? {0} ", !app_data.resumed);
 
-pub fn poll_all_ms(block: bool) -> Option<ndk_glue::Event> {
-    let looper = ThreadLooper::for_thread().unwrap();
-    let result = if block {
-        looper.poll_all()
-    } else {
-        looper.poll_all_timeout(std::time::Duration::from_millis(0u64))
+    let ctx = ALXRRustCtx {
+        graphicsApi: APP_CONFIG.graphics_api.unwrap_or(ALXRGraphicsApi::Auto),
+        decoderType: ALXRDecoderType::NVDEC, // Not used on android.
+        displayColorSpace: APP_CONFIG.color_space.unwrap_or(ALXRColorSpace::Default),
+        verbose: APP_CONFIG.verbose,
+        applicationVM: vm_ptr as *mut std::ffi::c_void,
+        applicationActivity: native_activity,
+        inputSend: Some(input_send),
+        viewsConfigSend: Some(views_config_send),
+        pathStringToHash: Some(path_string_to_hash),
+        timeSyncSend: Some(time_sync_send),
+        videoErrorReportSend: Some(video_error_report_send),
+        batterySend: Some(battery_send),
+        setWaitingNextIDR: Some(set_waiting_next_idr),
+        requestIDR: Some(request_idr),
+        disableLinearizeSrgb: APP_CONFIG.no_linearize_srgb,
+        noSuggestedBindings: APP_CONFIG.no_bindings,
+        noServerFramerateLock: APP_CONFIG.no_server_framerate_lock,
+        noFrameSkip: APP_CONFIG.no_frameskip,
+        disableLocalDimming: APP_CONFIG.disable_localdimming,
+        headlessSession: APP_CONFIG.headless_session,
+        firmwareVersion: get_firmware_version(&vm),
     };
-    match result {
-        Ok(Poll::Event { ident, .. }) => match ident {
-            ndk_glue::NDK_GLUE_LOOPER_EVENT_PIPE_IDENT => ndk_glue::poll_events(),
-            ndk_glue::NDK_GLUE_LOOPER_INPUT_QUEUE_IDENT => {
-                let input_queue = ndk_glue::input_queue();
-                let input_queue = input_queue.as_ref().expect("Input queue not attached!");
-                assert!(input_queue.has_events());
-                while let Some(event) = input_queue.get_event().unwrap() {
-                    if let Ok(Some(event)) =
-                        std::panic::catch_unwind(|| input_queue.pre_dispatch(event))
-                    {
-                        input_queue.finish_event(event, false);
-                    }
-                }
-                None
-            }
-            _ => unreachable!("Unrecognized looper identifer"),
-        },
-        _ => None,
+    let mut sys_properties = ALXRSystemProperties::new();
+    if !alxr_init(&ctx, &mut sys_properties) {
+        return Ok(());
     }
-}
 
-fn run(app_data: &mut AppData) -> Result<(), Box<dyn std::error::Error>> {
-    unsafe {
-        let ctx = ndk_context::android_context();
-        let native_activity = ctx.context();
-        let vm_ptr = ctx.vm();
-
-        let _lib = libloading::Library::new("libopenxr_loader.so")?;
-
-        let vm = jni::JavaVM::from_raw(vm_ptr.cast())?;
-        let _env = vm.attach_current_thread()?;
-
-        check_android_permissions(native_activity as jni::sys::jobject, &vm)?;
-
-        let ctx = ALXRRustCtx {
-            graphicsApi: APP_CONFIG.graphics_api.unwrap_or(ALXRGraphicsApi::Auto),
-            decoderType: ALXRDecoderType::NVDEC, // Not used on android.
-            displayColorSpace: APP_CONFIG.color_space.unwrap_or(ALXRColorSpace::Default),
-            verbose: APP_CONFIG.verbose,
-            applicationVM: vm_ptr as *mut std::ffi::c_void,
-            applicationActivity: native_activity,
-            inputSend: Some(input_send),
-            viewsConfigSend: Some(views_config_send),
-            pathStringToHash: Some(path_string_to_hash),
-            timeSyncSend: Some(time_sync_send),
-            videoErrorReportSend: Some(video_error_report_send),
-            batterySend: Some(battery_send),
-            setWaitingNextIDR: Some(set_waiting_next_idr),
-            requestIDR: Some(request_idr),
-            disableLinearizeSrgb: APP_CONFIG.no_linearize_srgb,
-            noSuggestedBindings: APP_CONFIG.no_bindings,
-            noServerFramerateLock: APP_CONFIG.no_server_framerate_lock,
-            noFrameSkip: APP_CONFIG.no_frameskip,
-            disableLocalDimming: APP_CONFIG.disable_localdimming,
-            headlessSession: APP_CONFIG.headless_session,
-            firmwareVersion: get_firmware_version(&vm),
-        };
-        let mut sys_properties = ALXRSystemProperties::new();
-        if !alxr_init(&ctx, &mut sys_properties) {
-            return Ok(());
-        }
-
-        if let Some((eye_w, eye_h)) = get_preferred_resolution(&vm, &sys_properties) {
-            println!("ALXR: Overriding recommend eye resolution ({}x{}) with prefferred resolution ({eye_w}x{eye_h})",
-                        sys_properties.recommendedEyeWidth, sys_properties.recommendedEyeHeight);
-            sys_properties.recommendedEyeWidth = eye_w;
-            sys_properties.recommendedEyeHeight = eye_h;
-        }
-        init_connections(&sys_properties);
-
-        while !app_data.destroy_requested {
-            // Main game loop
-            loop {
-                // event pump loop
-                let block =
-                    !app_data.destroy_requested && !app_data.resumed && !alxr_is_session_running();
-                // If the timeout is zero, returns immediately without blocking.
-                // If the timeout is negative, waits indefinitely until an event appears.
-                if let Some(event) = poll_all_ms(block) {
-                    app_data.handle_lifecycle_event(&event);
-                } else {
-                    break;
-                }
-            }
-            // update and render
-            let mut exit_render_loop = false;
-            let mut request_restart = false;
-            alxr_process_frame(&mut exit_render_loop, &mut request_restart);
-            if exit_render_loop {
-                break;
-            }
-        }
-
-        shutdown();
-        alxr_destroy();
+    if let Some((eye_w, eye_h)) = get_preferred_resolution(&vm, &sys_properties) {
+        log::info!("ALXR: Overriding recommend eye resolution ({}x{}) with prefferred resolution ({eye_w}x{eye_h})",
+                    sys_properties.recommendedEyeWidth, sys_properties.recommendedEyeHeight);
+        sys_properties.recommendedEyeWidth = eye_w;
+        sys_properties.recommendedEyeHeight = eye_h;
     }
+    init_connections(&sys_properties);
+
+    while !app_data.destroy_requested {
+        android_app.poll_events(NO_WAIT_TIME, |event| {
+            app_data.handle_lifecycle_event(&event);
+        });
+
+        let mut exit_render_loop = false;
+        let mut request_restart = false;
+        alxr_process_frame(&mut exit_render_loop, &mut request_restart);
+        if exit_render_loop {
+            break;
+        }
+    }
+
+    shutdown();
+    alxr_destroy();
     Ok(())
 }
